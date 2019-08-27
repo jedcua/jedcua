125d0c29205afd3d77ba177df0d9c5a047ed62de
Jed Cua <jonedwardcua@gmail.com>
2019-08-27 23:38:05 +0800

    Fixed deploy script, 3rd try

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,10 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
+  cd html
   git init
 
-  git remote add deploy "travis@jedcua.dev:/home/travis/jedcua"
+  git remote add deploy "travis@jedcua.dev:/var/www"
   git config user.name "Travis CI"
   git config user.email "jonedwardcua@gmail.com"
 
